5b91610f467a42b917b3ba62355409f27e9bf107
cylam0 <cylam0.hk@gmail.com>
2023-12-17 22:20:46 +0000

    update: output computations to txt file

--- index.mjs
+++ index.mjs
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 import TransactionLoader from "./class/TransactionLoader.mjs";
 import TransactionLogic from "./class/TransactionLogic.mjs";
 import TransactionPool from "./class/TransactionPool.mjs";
@@ -6,22 +8,38 @@ const transactionLogic = new TransactionLogic()
 
 const transactions = new TransactionLoader().load("./data/transactions.csv");
 
-const ticker = "VUSD";
+const disposedTickers = Array.from(new Set([
+  ...transactions.filter(t => t.share < 0)
+    .map(x => x.ticker)
+]))
 
-transactionLogic.computeMatches({ transactions, ticker });
+const outputFilePath = './output.txt';
 
-transactionLogic.logCalculations(
-  {
-    transactions,
-    ticker,
-    pool: new TransactionPool({
-      ticker,
-      share: 0,
-      cost: 0,
-    })
-  },
-  {
-    transactionLogic,
-    logger: console
+const logger = {
+  log(x) {
+    fs.writeFileSync(outputFilePath, x + '\n', { flag: 'a' });
   }
-);
+}
+
+fs.writeFileSync(outputFilePath, '', { flag: 'w' });
+
+for (const ticker of disposedTickers) {
+  transactionLogic.computeMatches({ transactions, ticker });
+  logger.log('\n');
+  logger.log(`Computations for ${ticker}\n`);
+  transactionLogic.logCalculations(
+    {
+      transactions,
+      ticker,
+      pool: new TransactionPool({
+        ticker,
+        share: 0,
+        cost: 0,
+      })
+    },
+    {
+      transactionLogic,
+      logger
+    }
+  )
+};
